29b1ef314f57e29b943b9f19302ac7a2cea0b292
Thomas Prochazka <thomas.prochazka@gmail.com>
2017-08-13 22:27:13 +0200

    Add LICENSE.md and do some repo housekeeping

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.css";
 export default class Footer extends Component {
     render() {
         return <footer className="Footer">
-            <p>If you find any issues or erros, please report them on the <a href="#">issue tracker.</a></p>
-            <p>This is a small side project by Thomas Prochazka. You can find the complete source code on <a href="#">Github</a>.</p>
+            <p>If you find any issues or erros, please report them on the <a href="https://github.com/zlypher/bool/issues">issue tracker.</a></p>
+            <p>This is a small side project by Thomas Prochazka. You can find the complete source code on <a href="https://github.com/zlypher/bool">Github</a>.</p>
         </footer>;
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,12 @@ import "./Header.css";
 export default class Header extends Component {
     render() {
         const { props } = this;
-        return <header>
-            <h1 className="Header">{props.content}</h1>
+        return <header className="Header">
+            <h1 className="Header-main">{props.content}</h1>
             <h2 className="Header-sub">{props.subheader}</h2>
+            <p className="Header-note">
+                This project is currently under development to create a first stable version. Currently expressions can only be entered with single char variables (a&&b, c||(d&&e)).
+            </p>
         </header>;
     }
 }
